f95fee064886df8c4ec1a69b639f45a4a8635e9d
joshlacey <williamjoshualacey@gmail.com>
2019-08-16 14:38:58 -0400

    added publish button

--- fe/src/Editor/Editor.component.js
+++ fe/src/Editor/Editor.component.js
@@ -1,17 +1,23 @@
-import React, {Fragment, useState} from 'react';
+import React, {useState} from 'react';
 import { 
   Markdown, 
   VerticalHandler, 
   Preview 
 } from './modules';
+import { saveMarkdownFiles } from '../utils/api';
 import './styles.scss';
 
 export default () => {
   const [markdown, setMarkdown] = useState('# Add Markdown Here');
   const [viewWidths, setViewWidths] = useState({left: 49, right: 49});
 
+  const publish = function() {
+    saveMarkdownFiles(markdown, 'Sally');
+  }
+
   return (
     <div className="editor">
+      <button style={{left: viewWidths.left - 6 + '%'}} onClick={publish}>Publish</button>
       {/* setting inline style. Eventually these will be set by a drag handler on the verticalhandler */}
       <Markdown {...{markdown, setMarkdown}} width={viewWidths.left}/>
       <VerticalHandler {...{viewWidths, setViewWidths}}/>
---
+++ fe/src/utils/api.js
@@ -0,0 +1,15 @@
+//const ES_API = "somewhereongoogle.com";
+
+function toJSON(response) {
+  // why do this: https://www.tjvantoll.com/2015/09/13/fetch-and-errors/
+  if(!response.ok) {
+    throw Error(response.statusText);
+  }
+  return response.json();
+}
+
+export function saveMarkdownFiles(body, user) {
+  alert(user, 'your markdown is all saved');
+  console.log(body);
+  //return fetch(ES_API).then(toJSON);
+}
