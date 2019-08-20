1671dc49eb3fe89679b843b028c9ce502661db44
joshlacey <williamjoshualacey@gmail.com>
2019-08-20 11:09:18 -0400

    added new route for markdown output

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Editor from './Editor';
+import MarkdownOutput from './MarkdownOutput';
+import {Route} from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <Editor />
+      <Route path="/markdown/edit" component={Editor} />
+      <Route path="/markdown" component={MarkdownOutput} /> 
     </div>
   );
 }
