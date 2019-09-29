019b4aa725077c65847e4afffd8e67f55d1e1b97
Cosmin Potocean <cosmycx@gmail.com>
2019-09-29 17:45:41 -0400

    time wait back on

--- be/main.go
+++ be/main.go
@@ -15,7 +15,7 @@ import (
 // main
 func main() {
 	// wait for elastic docker connection
-	time.Sleep(time.Second * 0)
+	time.Sleep(time.Second * 90)
 
 	// configure log
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
