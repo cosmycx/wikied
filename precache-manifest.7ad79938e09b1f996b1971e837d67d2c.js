072477bafecb6dd03b21fbc66ded1da863485c24
Cosmin Potocean <cosmycx@gmail.com>
2019-09-29 11:35:58 -0400

    Updates

---
+++ precache-manifest.7ad79938e09b1f996b1971e837d67d2c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e8cc1416d217dff8f14d767bacba24a",
+    "url": "/wikied/index.html"
+  },
+  {
+    "revision": "0e79d8831bf5532fd357",
+    "url": "/wikied/static/css/main.16101f8d.chunk.css"
+  },
+  {
+    "revision": "8248fc5c0e64ee744069",
+    "url": "/wikied/static/js/2.ca3dbe2d.chunk.js"
+  },
+  {
+    "revision": "0e79d8831bf5532fd357",
+    "url": "/wikied/static/js/main.d010fe86.chunk.js"
+  },
+  {
+    "revision": "9f340729ce18d5ec7e7e",
+    "url": "/wikied/static/js/runtime~main.81284401.js"
+  }
+]);
