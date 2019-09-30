71562505354c521abb84dbb82363c9fcd86a53f6
joshlacey <williamjoshualacey@gmail.com>
2019-09-30 10:26:25 -0400

    added docs to the commit

---
+++ docs/precache-manifest.ce629587de054b21e54fc1543f73f60d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c2f97e6d101dd62e987dd3dafa227a5",
+    "url": "/wikied/index.html"
+  },
+  {
+    "revision": "5257154a293afe3ffbbe",
+    "url": "/wikied/static/css/main.52492f9d.chunk.css"
+  },
+  {
+    "revision": "44dda8d5bbc9ad8bfac4",
+    "url": "/wikied/static/js/2.ba124ad1.chunk.js"
+  },
+  {
+    "revision": "5257154a293afe3ffbbe",
+    "url": "/wikied/static/js/main.2b583e8d.chunk.js"
+  },
+  {
+    "revision": "9f340729ce18d5ec7e7e",
+    "url": "/wikied/static/js/runtime~main.81284401.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/wikied/docs/precache-manifest.8317ea75f2b93c8d194867ee5586828b.js"
+  "/wikied/precache-manifest.ce629587de054b21e54fc1543f73f60d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/wikied/docs/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/wikied/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
